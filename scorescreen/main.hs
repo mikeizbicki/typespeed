10945480b5d071467009e2b3bc22642b0133faf9
cheripai <datdo@rocketmail.com>
2014-09-16 11:23:12 -0700

    update functions

--- scorescreen/main.hs
+++ scorescreen/main.hs
@@ -8,11 +8,15 @@ main = do
     contents <- fmap B.lines $ B.readFile "/usr/local/var/games/typespeed.score" 
     let score = wordsLines contents
     print score
-    return ()
 
 
 -- Calls words on all lines of a list
 -- Used to create 2d array out of list of scores
 wordsLines :: [B.ByteString] -> [[B.ByteString]]
-wordsLines [] = [[]]
+wordsLines [] = []
 wordsLines (x:xs) = [B.words x] ++ wordsLines xs
+
+
+-- Returns average score as a bytestring
+averageScore :: [[B.ByteString]] -> Int  
+averageScore [[]] = 0
