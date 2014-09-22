3a7a19217e00a7d01d4c7d2302b1b92f9b1a7724
cheripai <datdo@rocketmail.com>
2014-09-22 12:54:12 -0700

    added filter time functions

--- scorescreen/main.hs
+++ scorescreen/main.hs
@@ -1,6 +1,7 @@
 import qualified Data.ByteString.Char8 as B
 import           Data.List
 import           System.Posix.User
+import           Data.Time.Clock.POSIX
 
 {-
     TODO:
@@ -44,6 +45,12 @@ main = do
     let addicts = top10Addicts score
     print addicts
 
+    let week = 604800 -- length of a week in seconds
+    curTime <- fmap round getPOSIXTime -- gets the current time as an integer
+    let weekScore = filterTime week curTime score
+    putStr "\nScores of the week: \n"
+    print weekScore
+    
 
 -- Calls words on all lines of a list
 -- Used to create 2d array out of list of scores
@@ -97,13 +104,15 @@ getUser = B.unpack . getUserBS
 top10Addicts :: [[B.ByteString]] -> [(Int,B.ByteString)]
 top10Addicts x = take 10 $ reverse $ sort $ count $ map getUserBS x
     
+
 -- counts the number of times an item appears in a list
 instances :: (Eq a) => a -> [a] -> Int
 instances _ [] = 0
 instances x (y:ys)
-    | x ==y = 1 + instances x ys
+    | x == y = 1 + instances x ys
     | otherwise = instances x ys
     
+
 -- returns a list tuples of each member of a list along with the number of times
 -- it appears in that list, i.e. counts number of times each item appears in list
 count :: (Eq a) => [a] -> [(Int,a)]
@@ -111,3 +120,16 @@ count [] = []
 count (x:xs) = (totalNum,x) : (count filtered_xs)
     where filtered_xs = filter (/= x) xs
           totalNum = 1 + instances x xs
+
+
+-- returns the time that the score was recorded from a single entry
+getTime :: [B.ByteString] -> Int
+getTime [] = 0
+getTime x = read (B.unpack (x !! 7)) :: Int
+
+
+-- returns the scores that were recorded within the given period
+filterTime :: Int -> Int -> [[B.ByteString]] -> [[B.ByteString]]
+filterTime _ _ [] = []
+filterTime period current x = filter (group) x
+    where group x' = getTime x' > (current - period)
